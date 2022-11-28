2ea03ba325f2fe71b41b0718630a0da2bb892b38
Denys Kuz <dkuz@gomage.com>
2022-11-28 10:53:36 +0200

    Blank Venia Extension

--- templates/localExtension/intercept.js
+++ templates/localExtension/intercept.js
@@ -1,15 +1,2 @@
-function localIntercept(targets) {
-    targets.of('@magento/pwa-buildpack').specialFeatures.tap(flags => {
-        /**
-         *  Wee need to activated esModules and cssModules to allow build pack to load our extension
-         * {@link https://magento.github.io/pwa-studio/pwa-buildpack/reference/configure-webpack/#special-flags}.
-         */
-        flags[targets.name] = {
-            cssModules: true,
-            esModules: true,
-            graphqlQueries: true,
-            rootComponents: true
-        };
-    });
-}
+function localIntercept(targets) {}
 module.exports = localIntercept;
